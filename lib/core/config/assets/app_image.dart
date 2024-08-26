e05d87caa28f616900022032cf5e7a1a7d62e353
yisaq27 <yishakabraham321@gmail.com>
2024-08-26 22:50:37 +0300

    image

--- lib/core/config/assets/app_image.dart
+++ lib/core/config/assets/app_image.dart
@@ -1,6 +1,7 @@
 class AppImages {
-   static const String basepath = 'assets/images/';
+  static const String basepath = 'assets/images/';
   static const String introBG = '${basepath}intro_bg.png';
   static const String chooseModeBG = '${basepath}choose_mode_bg.png';
-
+  static const String homeArtistBG = '${basepath}home_artist.png';
+  static const String authBG = '${basepath}auth_bg.png';
 }
