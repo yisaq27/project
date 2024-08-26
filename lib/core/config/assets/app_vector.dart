fb81bd8b367ee0641cb2212ba7b881928e380a7d
yisaq27 <yishakabraham321@gmail.com>
2024-08-26 22:59:52 +0300

    logo

--- lib/core/config/assets/app_vector.dart
+++ lib/core/config/assets/app_vector.dart
@@ -1,7 +1,9 @@
 class AppVectors {
   static const String basepath = 'assets/vectors/';
   static const String format = '.svg';
+
   static const String logo = '${basepath}spotify_logo$format';
   static const String sun = '${basepath}sun$format';
   static const String moon = '${basepath}moon$format';
+  static const String topPatter = '${basepath}top-pattern$format';
 }
